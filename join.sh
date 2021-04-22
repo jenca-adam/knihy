0c04db31f80adfefa13360c7bc05cc0582f8b932
Adam Jenča <jenca.adam@gmail.com>
2021-04-22 08:38:23 +0200

    ch

--- join.sh
+++ join.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-pdfjam chapter*/chapter.pdf 
-mv worksheet-pdfjam.pdf book.pdf
-qpdf --rotate=-90 book.pdf --replace-input
+for i in chapter* ;do
+	cd $i;
+	pdfjam page?.pdf worksheet.pdf -o chapter.pdf;
+	qpdf --rotate=-90 chapter.pdf --replace-input;
+
+	cd /home/adam/skola/anglictina/pdf/books/oliver-twist ;
+	pwd;
+	done
+pdfjam chapter*/chapter.pdf -o book.pdf
 
